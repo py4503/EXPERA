ebd2e3545036458a4916e5f07a9efcb963dffe4d
py4503 <pratyushyaduwanshi980@gmail.com>
2025-07-14 23:44:29 +0530

    feat: integrate CKEditor 5 into RealTimeEditor component and update styles

--- EXPERA/src/App.jsx
+++ EXPERA/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import RealTimeEditor from './components/Editor/RealTimeEditor'
+import { useForm } from 'react-hook-form'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const {control} = useForm();
   return (
     <>
-    <div className='w-2xl'>
-    <RealTimeEditor/>
+    <div className='w-2xl rounded-2xl overflow-hidden border-2'>
+    <RealTimeEditor control={control}/>
     </div>
     </>
   )
--- EXPERA/src/components/Editor/RealTimeEditor.jsx
+++ EXPERA/src/components/Editor/RealTimeEditor.jsx
@@ -1,22 +1,338 @@
-import React from 'react'
-import {useEditor, EditorContent, extensions} from '@tiptap/react'
-import {StarterKit} from '@tiptap/starter-kit'
-import Underline from '@tiptap/extension-underline';
- 
-function RealTimeEditor() {
-        const editor = useEditor(
-            {
-                extensions:[StarterKit],
-                content: '<p className = "text-gray-600">Type your content here...</p>',
+import React, { useRef, useMemo, useEffect, useState } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import { Controller } from 'react-hook-form';
+
+import {
+  MediaEmbedUI,
+  ClassicEditor,
+  Alignment,
+  Autoformat,
+  AutoImage,
+  AutoLink,
+  Autosave,
+  AutoMediaEmbed,
+  BalloonToolbar,
+  BlockQuote,
+  Bold,
+  Bookmark,
+  CloudServices,
+  Code,
+  CodeBlock,
+  Emoji,
+  Essentials,
+  FindAndReplace,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  Fullscreen,
+  GeneralHtmlSupport,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  ImageBlock,
+  ImageCaption,
+  ImageEditing,
+  ImageInline,
+  ImageInsertViaUrl,
+  ImageResize,
+  ImageStyle,
+  ImageTextAlternative,
+  ImageToolbar,
+  ImageUpload,
+  ImageUtils,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  List,
+  ListProperties,
+  Markdown,
+  MediaEmbed,
+  Mention,
+  PageBreak,
+  Paragraph,
+  PasteFromMarkdownExperimental,
+  PasteFromOffice,
+  RemoveFormat,
+  SourceEditing,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+  Strikethrough,
+  Style,
+  Subscript,
+  Superscript,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  TextTransformation,
+  TodoList,
+  Underline,
+  WordCount
+} from 'ckeditor5';
+
+import 'ckeditor5/ckeditor5.css';
+
+function RTE({ name = 'content', control, label, defaultValue = '' }) {
+  const wordCountRef = useRef(null);
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+    return () => setReady(false);
+  }, []);
+
+  const { editorConfig } = useMemo(() => {
+    if (!ready) return {};
+    return {
+      editorConfig: {
+        toolbar: {
+          items: [
+            '|',
+            'undo',
+            'redo',
+            '|',
+            'sourceEditing',
+            '|',
+            'heading',
+            'style',
+            '|',
+            'fontSize',
+            'fontFamily',
+            'fontColor',
+            'fontBackgroundColor',
+            '|',
+            'bold',
+            'italic',
+            'underline',
+            '|',
+            'link',
+            'mediaEmbed',
+            'insertTable',
+            'highlight',
+            'blockQuote',
+            'codeBlock',
+            '|',
+            'alignment',
+            '|',
+            'bulletedList',
+            'numberedList',
+            'todoList',
+            'outdent',
+            'indent'
+          ],
+          shouldNotGroupWhenFull: false
+        },
+        plugins: [
+          MediaEmbedUI,
+          AutoMediaEmbed,
+          MediaEmbed,
+          Alignment,
+          Autoformat,
+          AutoImage,
+          AutoLink,
+          Autosave,
+          BalloonToolbar,
+          BlockQuote,
+          Bold,
+          Bookmark,
+          CloudServices,
+          Code,
+          CodeBlock,
+          Emoji,
+          Essentials,
+          FindAndReplace,
+          FontBackgroundColor,
+          FontColor,
+          FontFamily,
+          FontSize,
+          Fullscreen,
+          GeneralHtmlSupport,
+          Heading,
+          Highlight,
+          HorizontalLine,
+          ImageBlock,
+          ImageCaption,
+          ImageEditing,
+          ImageInline,
+          ImageInsertViaUrl,
+          ImageResize,
+          ImageStyle,
+          ImageTextAlternative,
+          ImageToolbar,
+          ImageUpload,
+          ImageUtils,
+          Indent,
+          IndentBlock,
+          Italic,
+          Link,
+          List,
+          ListProperties,
+          Markdown,
+          MediaEmbed,
+          Mention,
+          PageBreak,
+          Paragraph,
+          PasteFromMarkdownExperimental,
+          PasteFromOffice,
+          RemoveFormat,
+          SourceEditing,
+          SpecialCharacters,
+          SpecialCharactersArrows,
+          SpecialCharactersCurrency,
+          SpecialCharactersEssentials,
+          SpecialCharactersLatin,
+          SpecialCharactersMathematical,
+          SpecialCharactersText,
+          Strikethrough,
+          Style,
+          Subscript,
+          Superscript,
+          Table,
+          TableCaption,
+          TableCellProperties,
+          TableColumnResize,
+          TableProperties,
+          TableToolbar,
+          TextTransformation,
+          TodoList,
+          Underline,
+          WordCount
+        ],
+        balloonToolbar: ['bold', 'italic', '|', 'link', '|', 'bulletedList', 'numberedList'],
+        fontFamily: { supportAllValues: true },
+        fontSize: {
+          options: [10, 12, 14, 'default', 18, 20, 22],
+          supportAllValues: true
+        },
+        heading: {
+          options: [
+            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+            { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+          ]
+        },
+        image: {
+          toolbar: [
+            'mediaEmbed',
+            'toggleImageCaption',
+            'imageTextAlternative',
+            '|',
+            'imageStyle:inline',
+            'imageStyle:wrapText',
+            'imageStyle:breakText',
+            '|',
+            'resizeImage'
+          ]
+        },
+        mediaEmbed:{
+          previewsInData: true
+        },
+        htmlSupport: {
+  allow: [
+    {
+      name: /.+/,
+      attributes: true,
+      classes: true,
+      styles: true
+    }
+  ]
+},
+        placeholder: 'Type or paste your content here!',
+        licenseKey: 'GPL',
+        link: {
+          addTargetToExternalLinks: true,
+          defaultProtocol: 'https://',
+          decorators: {
+            toggleDownloadable: {
+              mode: 'manual',
+              label: 'Downloadable',
+              attributes: {
+                download: 'file'
+              }
             }
-        );
+          }
+        },
+        list: {
+          properties: {
+            styles: true,
+            startIndex: true,
+            reversed: true
+          }
+        },
+        mention: {
+          feeds: [
+            {
+              marker: '@',
+              feed: []
+            }
+          ]
+        },
+        style: {
+          definitions: [
+            { name: 'Title', element: 'h2', classes: ['document-title'] },
+            { name: 'Subtitle', element: 'h3', classes: ['document-subtitle'] },
+            { name: 'Info box', element: 'p', classes: ['info-box'] },
+            { name: 'CTA Link Primary', element: 'a', classes: ['button', 'button--green'] },
+            { name: 'Marker', element: 'span', classes: ['marker'] }
+          ]
+        },
+        uiColor:'1e1e1e',
+        table: {
+          contentToolbar: [
+            'tableColumn',
+            'tableRow',
+            'mergeTableCells',
+            'tableProperties',
+            'tableCellProperties'
+          ]
+        }
+      }
+    };
+  }, [ready]);
+
   return (
-    <div>
-    <div className="border p-4 rounded">
-        <EditorContent editor={editor} />
+    <div className="w-full rounded-2xl">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+      <Controller
+        name={name}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field: { onChange, value } }) => (
+          <CKEditor
+            editor={ClassicEditor}
+            config={editorConfig}
+            data={value}
+            onChange={(_, editor) => {
+              const data = editor.getData();
+              onChange(data); // updates form state
+            }}
+            // word count
+            // onReady={(editor) => {
+            //   const wordCount = editor.plugins.get('WordCount');
+            //   if (wordCountRef.current && wordCount.wordCountContainer) {
+            //     wordCountRef.current.innerHTML = '';
+            //     wordCountRef.current.appendChild(wordCount.wordCountContainer);
+            //   }
+            // }}
+          />
+        )}
+      />
+      <div ref={wordCountRef} className="mt-2 text-sm text-gray-600" />
     </div>
-    </div>
-  )
+  );
 }
 
-export default RealTimeEditor
+export default RTE;
